5f5bb1bfbc548fae125da24ec17b3bd481ba14c0
Bruno <bruno03araujo@hotmail.com>
2020-10-27 15:35:13 -0300

    apagando comentarios

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -23,27 +23,27 @@ public class Aplicacao {
 		map.put(1, "def");
 		map.put(2, "456");
 		
-		/*
-		 * System.out.println("Operações da lista\n");
-		 * 
-		 * lista1 = lista; System.out.println(lista.equals(lista1));
-		 * System.out.println(lista.contains("bruno"));
-		 * System.out.println(lista.contains("abc"));
-		 * System.out.println(lista.isEmpty()); System.out.println(lista.size());
-		 * System.out.println(lista.get(0)); System.out.println(lista.remove(2));
-		 * System.out.println(lista.size()); lista.clear();
-		 * System.out.println(lista.size());
-		 * 
-		 * System.out.println("\nOperações do map\n"); map1 = map;
-		 * System.out.println(map.equals(map1));
-		 * System.out.println(map.containsValue("pedro"));
-		 * System.out.println(map.containsValue("def")); System.out.println(map.get(0));
-		 * System.out.println(map.remove("456")); System.out.println(map.size());
-		 * System.out.println(map.isEmpty());
-		 * System.out.println(map.values().toString()); map.clear();
-		 * System.out.println(map.size());
-		 */
-		System.out.println(map.get(0));
+		
+		  System.out.println("Operações da lista\n");
+		  
+		  lista1 = lista; System.out.println(lista.equals(lista1));
+		  System.out.println(lista.contains("bruno"));
+		  System.out.println(lista.contains("abc"));
+		  System.out.println(lista.isEmpty()); System.out.println(lista.size());
+		  System.out.println(lista.get(0)); System.out.println(lista.remove(2));
+		  System.out.println(lista.size()); lista.clear();
+		  System.out.println(lista.size());
+		  
+		  System.out.println("\nOperações do map\n"); map1 = map;
+		  System.out.println(map.equals(map1));
+		  System.out.println(map.containsValue("pedro"));
+		  System.out.println(map.containsValue("def")); System.out.println(map.get(0));
+		  System.out.println(map.remove("456")); System.out.println(map.size());
+		  System.out.println(map.isEmpty());
+		  System.out.println(map.values().toString()); map.clear();
+		  System.out.println(map.size());
+		 
+		
 	}
 
 }
